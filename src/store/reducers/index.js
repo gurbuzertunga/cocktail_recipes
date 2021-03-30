7a8512233c011751179f5a8609572f9474e85ea2
gurbuzertunga <gurbuzertunga@gmail.com>
2021-03-30 13:54:37 +0300

    combine reducers

---
+++ src/store/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import categoriesReducer from './categoriesReducer';
+import filterReducer from './filterReducer';
+import recipesReducer from './recipesReducer';
+import urlReducer from './urlReducer';
+
+
+const rootReducer = combineReducers({
+  data: recipesReducer,
+  categories: categoriesReducer,
+  filter: filterReducer,
+  url: urlReducer,
+});
+
+export default rootReducer;
