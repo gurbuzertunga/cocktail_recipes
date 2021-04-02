e728673f11eb2a247f6d7349bd28554cc2e6f3f0
gurbuzertunga <gurbuzertunga@gmail.com>
2021-04-02 20:54:05 +0300

    update readme and finalize build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,10 @@ const Footer = () => (
   <footer>
     <div className={style.container}>
       <p>
+        Data Provided by
+        <a href="https://www.thecocktaildb.com/">Cocktails DB</a>
+      </p>
+      <p>
         Made by
         <a href="https://gurbuzertunga.site/" target="_blank" rel="noreferrer">Gurbuz Ertunga</a>
       </p>
